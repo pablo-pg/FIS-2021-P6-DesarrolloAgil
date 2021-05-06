31c31c4099f9895f26e1822c4f6df74832fa8ba8
AndPerCast <alu0101313511@ull.edu.es>
2021-05-06 15:16:50 +0100

    Arreglo del script de testing

--- code/tools/run_tests.sh
+++ code/tools/run_tests.sh
@@ -7,13 +7,13 @@ if cd ../test; then
   passed=0
   total=0
   # Crea una lista con todos los tests.
-  for $test in $(ls test*) do;
+  for test in $(ls test*); do
     # Revisa si puede ejecutarse y en tal caso, si tiene éxito.
-    if [ -x $test ] && ./$test; then
-      passed=$((passed + 1))
+    if [ -x $test ]; then
+      ./$test && passed=$((passed + 1))
+      total=$((total + 1))
     fi
-    total=$((total + 1))
   done
   # Imprime el recuento de pruebas superadas con éxito.
-  echo -e "\nTest superados correctamente: ${passed}/${total}"
+  echo -e "\nPruebas superadas correctamente: ${passed}/${total}"
 fi
