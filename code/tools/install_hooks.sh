b0bd806b1454fe89512221cf652b90b85cf09a54
Pablo <alu0101318318@ull.edu.es>
2021-05-20 21:28:13 +0100

    Test

--- code/tools/install_hooks.sh
+++ code/tools/install_hooks.sh
@@ -2,8 +2,8 @@
 
 # Instala y activa el servicio de Git Hooks de forma local.
 
-GIT_DIR=$(git rev-parse --git-dir)
-CUR_DIR=$(pwd)
+GIT_DIR="$(git rev-parse --git-dir)"
+CUR_DIR="$(pwd)"
 
 echo "Instalando Git Hooks..."
 # Crea un link simbólico en la carpeta local (normalmente ./git/hooks).
